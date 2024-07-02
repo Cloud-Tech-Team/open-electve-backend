531c8fc08621c301a6eb7c1cbd6925f4282d6be7
Glitchyi <advaithnarayanan8@gmail.com>
2024-07-02 20:18:34 +0530

    updated to work properly

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -4,17 +4,23 @@ const Course = require("../models/course.model");
 
 app.post("/create", async (req, res) => {
   const course = new Course(req.body);
-  const response = Course.findOne({ courseCode: course.courseCode })
-  // if (response) {
-  //   console.log(response);
-  //   return res.status(412).send("Course already exists");
-  // }
-  course.save().then((result) => {
-    if (!result) {
+  try {
+    const existingCourse = await Course.findOne({courseCode: course.courseCode});
+    if (existingCourse) {
+      return res.status(412).send("Course already exists");
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+  course.save().then(async (result) => {
+    if (result) {
+      const check = await Course.findOne(course);
       return res.sendStatus(200);
     }
     return res.status(400);
   });
+
 });
 
 app.get("/admin", (req, res) => {
